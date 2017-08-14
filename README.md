0db94a49c7c50ef83f9a63ec1aa58d60faed6cbf
anark1 <anark1@protonmail.ch>
2017-08-14 22:41:04 +0700

    fun

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # crusader.ru
 
-About your mother
+This is repository for crusader developers making official website.
+
+Check out our vk group: vk.com/crusader_web
+Contact us with telegram: **admin's contact here**
--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Crusader TM</title>
+	<title>Crusader</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="js/script.js" defer></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 </head>
 <body>
-	
+	<a href="https://vk.com/crusader_web">Go to vk group</a>	
 </body>
 </html>
