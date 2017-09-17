90085ebd75c187e8880852827664126d45daa58d
deixman <deix@yandex.ru>
2017-09-18 02:00:30 +0700

    work in progress

--- index.php
+++ index.php
@@ -8,6 +8,53 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 </head>
 <body>
-	
+	<div id="header-bg-main"></div>
+	<div id="header-bg-menu1"></div>
+	<div id="header-bg-menu2"></div>
+	<div id="header">
+		<div class="row black">
+			<div class="column textBlue">
+				<span>для звонков по России</span>
+				<span style="font-size: 20pt">+7 963 193 77 38<img src="img/Phone.png" alt=""></span>
+			</div>
+			<div class="row">
+				<div><img src="img/VK.png" alt=""><a class="textBlue" href="https://vk.com/crusader_web">vk.com</a></div>
+				<div><img src="img/Telegram.png" alt=""><a class="textBlue" href="https://t.me/joinchat/AAAAAELOeY4ok1Pjom8FsA">Telegram</a></div>
+			</div>
+		</div>
+		<div class="row black opacity">
+			<div><a href="/"><img src="img/logo.png" alt=""></a></div>
+			<div class="row">
+				<div>			
+					<a class="textWhite" href="/">
+						Main
+					</a>
+				</div>
+				<div>
+					<a class="textWhite" href="">
+						button1
+					</a>
+				</div>
+				<div>
+					<a class="textWhite" href="">
+						button2
+					</a>
+				</div>
+				<div>
+					<a class="textWhite" href="">
+						button3
+					</a>
+				</div>
+				<div>
+					<a class="textWhite" href="/about">
+						About
+					</a>
+				</div>
+			</div>
+		</div>
+		<div>
+			div*3
+		</div>
+	</div>
 </body>
 </html>
