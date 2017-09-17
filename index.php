bb03394e21b20e9a35f3d9cdf30e7efb15a16a80
deixman <deix@yandex.ru>
2017-09-18 06:41:49 +0700

    Серый доделает там щрифты и прочую мелкую хуйню
    я заебался

--- index.php
+++ index.php
@@ -8,49 +8,76 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 </head>
 <body>
-	<div id="header">
-		<div class="row black">
-			<div class="column textBlue">
-				<span>для звонков по России</span>
-				<span style="font-size: 20pt">+7 963 193 77 38<img src="img/Phone.png" alt=""></span>
+	<div class="bg black" id="bg-header-first"></div>
+	<div class="bg black opacity" id="bg-header-second"></div>
+	<div class="bg" id="bg-header-third"></div>
+	<div class="bg" id="bg-content-first"></div>
+	<div id="wrapper">
+		<div id="header">
+			<div class="row">
+				<div class="column textBlue">
+					<span>для звонков по России</span>
+					<span>+7 963 193 77 38<img src="img/Phone.png" alt=""></span>
+				</div>
+				<div class="row middle">
+					<div><img src="img/VK.png" alt=""><a class="textBlue" href="https://vk.com/crusader_web">vk.com</a></div>
+					<div><img src="img/Telegram.png" alt=""><a class="textBlue" href="https://t.me/joinchat/AAAAAELOeY4ok1Pjom8FsA">Telegram</a></div>
+				</div>
 			</div>
 			<div class="row">
-				<div><img src="img/VK.png" alt=""><a class="textBlue" href="https://vk.com/crusader_web">vk.com</a></div>
-				<div><img src="img/Telegram.png" alt=""><a class="textBlue" href="https://t.me/joinchat/AAAAAELOeY4ok1Pjom8FsA">Telegram</a></div>
+				<div><a href="/"><img src="img/logo.png" alt=""></a></div>
+				<div class="row middle">
+					<div>			
+						<a class="textWhite" href="/">
+							Main
+						</a>
+					</div>
+					<div>
+						<a class="textWhite" href="">
+							button1
+						</a>
+					</div>
+					<div>
+						<a class="textWhite" href="">
+							button2
+						</a>
+					</div>
+					<div>
+						<a class="textWhite" href="">
+							button3
+						</a>
+					</div>
+					<div>
+						<a class="textWhite" href="/about">
+							About
+						</a>
+					</div>
+				</div>
+			</div>
+			<div id="third" class="column center middle">
+				<div class="column middle" style="margin: auto;">
+					<span class="textBlue" style="font-size: 72px">Мы создаем уникальные сайты</span>
+					<span class="textWhite" style="font-size: 72px">такими, каким вы его видите</span>
+				</div>
+				<div id="purchase" style="margin: auto;"><span>PURCHASE NOW</span></div>
+				<div style="margin: auto;"><img src="img/unknow.png" alt=""></div>
 			</div>
 		</div>
-		<div class="row black opacity">
-			<div><a href="/"><img src="img/logo.png" alt=""></a></div>
+		<div id="content">
 			<div class="row">
-				<div>			
-					<a class="textWhite" href="/">
-						Main
-					</a>
+				<div class="textWhite" style="font-size: 36px">Больше информации по тел.
+					<br>+7 963 193 77 38
 				</div>
-				<div>
-					<a class="textWhite" href="">
-						button1
-					</a>
-				</div>
-				<div>
-					<a class="textWhite" href="">
-						button2
-					</a>
-				</div>
-				<div>
-					<a class="textWhite" href="">
-						button3
-					</a>
-				</div>
-				<div>
-					<a class="textWhite" href="/about">
-						About
-					</a>
+				<div class="textWhite" style="font-size: 36px; text-align: right;">
+					Свяжитесь с нами в соц. сетях:<br>vk.com<br>Telegram
 				</div>
 			</div>
-		</div>
-		<div>
-			div*3
+			<div class="row center textWhite" style="font-size: 24px">
+				<span>Полная версия сайта закрыта на техническое обслуживание. Приносим свои извинения.</span>
+			</div>
+			<div class="row center textWhite" style="font-size: 14px">
+				<span>2017 Crusader Team</span>
+			</div>
 		</div>
 	</div>
 </body>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-
+var w = window.innerWidth;
+var h = window.innerHeight;
+document.querySelector('#header').style.height = h+'px';
+var bgf = document.querySelector('#header > div:nth-child(1)').offsetHeight;
+document.querySelector('#bg-header-first').style.height = bgf+'px';
+var bgs = document.querySelector('#header > div:nth-child(2)').offsetHeight;
+document.querySelector('#bg-header-second').style.height = bgs+'px';
+document.querySelector('#bg-header-second').style.top = bgf+'px';
+document.querySelector('#bg-header-third').style.height = h+'px';
+var bgsf = document.querySelector('#content').offsetHeight;
+document.querySelector('#bg-content-first').style.top = h+'px';
+document.querySelector('#bg-content-first').style.height = bgsf+'px';
+//console.log(w);
+switch (true) {
+	case w < 1024:
+		document.getElementById('wrapper').style.width = '760px';
+		break;
+	case w < 760:
+		document.getElementById('wrapper').style.width = '424px';
+		break;
+	case w < 424:
+		document.getElementById('wrapper').style.width = '374px';
+		break;
+	case w < 374:
+		document.getElementById('wrapper').style.width = '319px';
+		break;
+}
